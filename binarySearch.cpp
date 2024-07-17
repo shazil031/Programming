4233acf4eaaa3f24d1c45b68d2f5468c2af5471b
shazil031 <mshazil151@gmail.com>
2024-07-17 18:19:47 +0530

    Update binarySearch.cpp

--- binarySearch.cpp
+++ binarySearch.cpp
@@ -1,11 +1,12 @@
 #include<iostream>
 using namespace std;
+
 int binarySearch(int arr[], int size, int key) {
 
     int start = 0;
     int end = size-1;
 
-    int mid = (start+end)/2;
+    int mid = start + (end-start)/2;
 
     while(start <= end) {
 
@@ -20,9 +21,10 @@ int binarySearch(int arr[], int size, int key) {
             end = mid - 1;
         }
 
-        mid = (start+end)/2;
+        mid = start + (end-start)/2;
 
     }
+    
     return -1;
 
 }
@@ -34,13 +36,13 @@ int main() {
     int even[6] = {2,4,6,8,12,18};
     int odd[5] = {3, 8, 11, 14, 16};
 
-    int evenIndex = binarySearch(even, 6, 20);
+    int evenIndex = binarySearch(even, 6, 6);
 
-    cout << " Index of 20 is " << evenIndex << endl;
+    cout << " Index of 6 is " << evenIndex << endl;
 
-    int oddIndex = binarySearch(odd, 5, 8);
+    int oddIndex = binarySearch(odd, 5, 14);
     
-    cout << " Index of 8 is " << oddIndex << endl;
+    cout << " Index of 14 is " << oddIndex << endl;
 
 
 
