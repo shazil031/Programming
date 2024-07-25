634816c2ff8ac0fb013138cd950322f07cd1f1c3
shazil031 <mshazil151@gmail.com>
2024-07-25 17:21:39 +0530

    Create Queue.cpp

---
+++ Queue.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<queue>
+
+using namespace std;
+int main() {
+
+    queue<string> q;
+    
+    q.push("love");
+    q.push("babbar");
+    q.push("kumar");
+    
+    cout << "Size before pop " << q.size() << endl;
+    cout << "Frist element: " << q.front() << endl;
+    q.pop();
+    cout << "Frist element: " << q.front() << endl;
+
+    cout << "Size after pop " << q.size() << endl;
+
+    return 0;
+
+}
