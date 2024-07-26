64ca4ec4ff6c04fa70450f77d7906016ce784633
shazil031 <mshazil151@gmail.com>
2024-07-26 23:47:28 +0530

    Update checkSortedRotatedArray.cpp

--- checkSortedRotatedArray.cpp
+++ checkSortedRotatedArray.cpp
@@ -1,5 +1,29 @@
 #include<iostream>
 using namespace std;
+char toLowerCase(char ch) {
+    if(ch>='a' && ch<='z') {
+        return ch;
+    }
+    else {
+        char temp =ch - 'A' + 'a';
+        return temp;
+    }
+}
+bool checkPalindrome(char a[], int n) {
+    int s = 0;
+    int e = n-1;
+
+    while(s<e) {
+        if(toLowerCase( a[s] ) != toLowerCase( a[e] ) ) {
+            return 0;
+        }
+        else {
+            s++;
+            e--;
+        }
+    }
+    return 1;
+}
 
 void reverse(char name[], int n) {
     int s= 0;
@@ -31,8 +55,14 @@ int main() {
     cout << "Your name is " << name << endl;
     int len = getLength(name);
     cout << "Length: " << len << endl;
+    
     reverse(name, len);
     cout << "Your name is " << name << endl;
+
+    cout << "Palindrome or not: " << checkPalindrome(name, len) << endl;
+
+    cout << " CHARACTER is " << toLowerCase('b') << endl;
+    cout << " CHARACTER is " << toLowerCase('D') << endl;
 
     return 0;
 }
