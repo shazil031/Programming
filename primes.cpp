95158be32004f169189112a34b84fb19c8428603
shazil031 <mshazil151@gmail.com>
2024-07-27 21:37:54 +0530

    Create primes.cpp

---
+++ primes.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+bool isPrime(int n) {
+    if(n<=1) {
+        return false;
+    }
+    for(int i = 2; i*i <= n; i++) {
+        if(n%i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main() {
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+
+    if(isPrime(n)) {
+        cout << "Prime" << endl;
+    }
+    else {
+        cout << "Not Prime" << endl;
+    }
+   
+    return 0;
+}
