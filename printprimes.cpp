c56aba853b551447e307c7ef23034933f0d3f086
shazil031 <mshazil151@gmail.com>
2024-08-01 22:11:31 +0530

    Update printprimes.cpp

--- printprimes.cpp
+++ printprimes.cpp
@@ -11,14 +11,13 @@ int main() {
         for(i=2;i<num;i++) {
             break;
         }
-    }
-    
+    } 
     int i=2;
     int num;
     if(i==num) {
-        cout<<num<<endl;
+        cout<<num<<endl; 
     }
-    
+     
     return 0;
 
 }
