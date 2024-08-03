a1bdf5068c8fc1021fa317d59fd81364285f1e6b
shazil031 <mshazil151@gmail.com>
2024-08-04 00:26:10 +0530

    Update palindrome.cpp

--- palindrome.cpp
+++ palindrome.cpp
@@ -11,10 +11,12 @@ int main() {
         for(j=1;j<=n-i;j++){
             cout<<"  ";
         }
+        
         int k=i;
         for(;j<=n;j++){
             cout<<k--<<" ";
         }
+        
         k=2;
         for(;j<=n+i-1;j++){
             cout<<k++<<" ";
