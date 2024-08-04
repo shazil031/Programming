b3710fd5f332965a423c8dc1a8fcea622a69f9c9
shazil031 <mshazil151@gmail.com>
2024-08-04 22:38:11 +0530

    Create pointers.cpp

---
+++ pointers.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+    int num = 5;
+
+    cout << num << endl;
+
+    // address of operator - &
+
+    cout << "Address of num is " << &num << endl;
+
+    int *ptr = &num;
+
+    cout << "value is : " << *ptr << endl;
+    cout << "Address is : " << ptr << endl;
+
+    double d = 3.14;
+    double *p2 = &d;
+
+    cout << "value is : " << *p2 << endl;
+    cout << "Address is : " << p2 << endl;
+
+
+    
+    return 0;
+}
