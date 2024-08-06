c02295f733c42a6ba65dbe11e850a69b9d89297f
shazil031 <mshazil151@gmail.com>
2024-08-06 19:58:55 +0530

    Update pointer.cpp

--- pointer.cpp
+++ pointer.cpp
@@ -41,8 +41,12 @@ int main() {
     //important concept
     int i = 3;
     int *t = &i;
-    cout << (*t)++ << endl;
-    // *t = *t + 1
+    //cout << (*t)++ << endl;
+    *t = *t + 1;
+    cout << *t << endl;
+    cout << "before t " << t << endl;
+    t = t + 1;
+    cout << "after t " << t << endl;
 
 
     return 0;
