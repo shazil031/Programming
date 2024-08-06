1970f739f9ad568f24292014d7bbbe203aa9f35d
shazil031 <mshazil151@gmail.com>
2024-08-06 20:37:14 +0530

    Create pointer2.cpp

---
+++ pointer2.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+    int arr[10];
+
+    cout <<"address of first memory block is " << arr << endl;
+    cout <<"address of first memory bolck is " << &arr[0] << endl;
+
+    return 0;
+}
