628a9c8631c955c3411755bd013f09d1c770fbad
shazil031 <mshazil151@gmail.com>
2024-08-06 21:53:03 +0530

    Update pointer2.cpp

--- pointer2.cpp
+++ pointer2.cpp
@@ -5,6 +5,7 @@ int main() {
 
     int arr[10] = {23, 122, 41, 67};
 
+    /*
     cout <<"address of first memory block is " << arr << endl;
     cout <<"address of first memory bolck is " << &arr[0] << endl;
 
@@ -15,8 +16,17 @@ int main() {
     cout << "8th " << arr[2] << endl;
     cout << "9th " << *(arr + 2) << endl;
     
+
     int i = 3;
     cout << i[arr] << endl;
+    */
 
+   int temp[10];
+   cout << sizeof(temp) << endl;
+
+   int *ptr = &temp[0];
+   cout << sizeof(ptr) << endl;
+
+   
     return 0;
 }
