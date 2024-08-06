559ddac1e72d52839b44f31f0d5e04134c9f4924
shazil031 <mshazil151@gmail.com>
2024-08-06 21:59:05 +0530

    Update pointer2.cpp

--- pointer2.cpp
+++ pointer2.cpp
@@ -21,12 +21,16 @@ int main() {
     cout << i[arr] << endl;
     */
 
-   int temp[10];
+   int temp[10] = {1,3};
    cout << sizeof(temp) << endl;
-
+   cout << "1st " << sizeof(*temp) << endl;
+   cout << "2nd " << sizeof(&temp) << endl;
+   
    int *ptr = &temp[0];
    cout << sizeof(ptr) << endl;
+   cout << sizeof(*ptr) << endl;
+   cout << sizeof(&ptr) << endl;
 
-   
+
     return 0;
 }
