d39d002eebc6a7ef6e268ea4e3f9ff3e462bff86
shazil031 <mshazil151@gmail.com>
2024-08-09 19:51:04 +0530

    Update pointer2.cpp

--- pointer2.cpp
+++ pointer2.cpp
@@ -34,9 +34,14 @@ int main() {
     */
 
    int a[20] = {1,2,3,5};
-   cout << &a[0] << endl;
-   cout << &a << endl;
-   cout << a << endl;
+   cout << " -> " << &a[0] << endl;
+   //cout << &a << endl;
+   //cout << a << endl;
+
+   int *p = &a[0];
+    cout << " -> " << p << endl;
+    //cout << *p << endl;
+    //cout << &p << endl;
 
     return 0;
 }
