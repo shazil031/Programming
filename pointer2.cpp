91184d66e7752f3f6abbb3f46734797ef9bf8141
shazil031 <mshazil151@gmail.com>
2024-08-09 20:55:10 +0530

    Update pointer2.cpp

--- pointer2.cpp
+++ pointer2.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main() {
 
-    int arr[10] = {23, 122, 41, 67};
+    //int arr[10] = {23, 122, 41, 67};
 
     /*
     cout <<"address of first memory block is " << arr << endl;
@@ -31,7 +31,6 @@ int main() {
    cout << sizeof(*ptr) << endl;
    cout << sizeof(&ptr) << endl;
     
-    */
 
    int a[20] = {1,2,3,5};
    cout << " -> " << &a[0] << endl;
@@ -42,6 +41,20 @@ int main() {
     cout << " -> " << p << endl;
     //cout << *p << endl;
     //cout << &p << endl;
+    cout << "-> " << &p << endl;
+
+    */
+   
+   int arr[10];
+
+//ERROR
+    //arr = arr + 1;
+
+    int *ptr = &arr[0];
+    cout << ptr << endl;
+    ptr = ptr + 1;
+    cout << ptr << endl;
+
 
     return 0;
 }
