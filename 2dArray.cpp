34dd5950089469b6ef916d5a47e7d3a85d1d9943
shazil031 <mshazil151@gmail.com>
2024-08-15 20:00:46 +0530

    Update 2dArray.cpp

--- 2dArray.cpp
+++ 2dArray.cpp
@@ -4,26 +4,29 @@ using namespace std;
 
 int main() {
 
-    int n;
-    cin >> n;
+    int row;
+    cin >> row;
+
+    int col;
+    cin >> col;
 
     //creating a 2D array
-    int** arr = new int* [n];
-    for(int i=0;i<n;i++){
-        arr[i] = new int[n];
+    int** arr = new int* [row];
+    for(int i=0;i<row;i++){
+        arr[i] = new int[row];
     }
     
     //taking input
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++) {
+    for(int i=0;i<row;i++){
+        for(int j=0;j<col;j++) {
             cin >> arr[i][j];
         }
     }
 
     //taking output
     cout << endl;
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++) {
+    for(int i=0;i<row;i++){
+        for(int j=0;j<col;j++) {
             cout << arr[i][j] << " ";
         }cout << endl;
     }
