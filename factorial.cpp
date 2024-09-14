cda99ac002cac91f501026a2df2dcc42919f4789
shazil031 <mshazil151@gmail.com>
2024-09-14 11:14:14 +0530

    Update factorial.cpp

--- factorial.cpp
+++ factorial.cpp
@@ -6,12 +6,10 @@ int factorial(int n) {
     //base case
     if(n == 0)
         return 1;
-
-    int chotti = factorial(n - 1);
-    int badi = n * chotti;
-
-    return badi;
+    int smallerProblem = factorial(n - 1);
+    int biggerProblem = n * smallerProblem;
     
+    return biggerProblem;;
 }
 
 int main() {
