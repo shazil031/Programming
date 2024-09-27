de8996f05734eeb4070e4619bb2044d0df494034
shazil031 <mshazil151@gmail.com>
2024-09-27 09:24:32 +0530

    Create OOPs.cpp

---
+++ OOPs.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+class Hero {
+
+    //properties
+    int health;
+
+};
+
+int main() {
+
+    //creation of object
+    Hero h1;
+
+    cout << "size : " << sizeof(h1) << endl;
+
+
+    return 0;
+}
