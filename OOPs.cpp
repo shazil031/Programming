4bce147fed0a2f9e912f580cda925998e5b9c624
shazil031 <mshazil151@gmail.com>
2024-10-19 00:02:36 +0530

    Update OOPs.cpp

--- OOPs.cpp
+++ OOPs.cpp
@@ -4,17 +4,45 @@ using namespace std;
 class Hero {
 
     //properties
+    private:
     int health;
+
+    public:
+    char level;
+
+    void print(){
+        cout << level << endl;
+    }
+
+    int gethealth() {
+        return health;
+    }
+
+    char getlevel() {
+        return level;
+    }
+
+    void sethealth(int h) {
+        health = h;
+    }
+
+    void setlevel(char ch) {
+        level = ch;
+    }
+
 
 };
 
 int main() {
 
     //creation of object
-    Hero h1;
+    Hero ramesh;
+    cout << "Ramesh health is " << ramesh.gethealth() << endl;
+    ramesh.sethealth(70);
+    ramesh.level = 'A';
 
-    cout << "size : " << sizeof(h1) << endl;
-
+    cout << "health is: " << ramesh.gethealth() << endl;
+    cout << "level is: " << ramesh.level << endl;
 
     return 0;
 }
